d14b6fd6ab2104a2c032e2c1c8eb7b3f213efddb
Simurg <andrzej.laskawiec@gmail.com>
2017-05-14 19:09:06 +0200

    second commit - branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@
  * Created by simurg on 2017-05-14.
  */
 public class Main {
-    // the beginning - adding something in the master (additional master text)
+    // the beginning - adding something in the branch + (additional master text)
     // first commit
     // first commit in branch1
 }
