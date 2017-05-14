01f0c24561628e0b421e08ec4b5857e16eff4f55
Mare-Tranquillitatis <andrzej.laskawiec@gmail.com>
2017-05-14 19:23:54 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@
  * Created by simurg on 2017-05-14.
  */
 public class Main {
-    // the beginning - adding something in the master
+    // the beginning - adding something in the master (additional master text)
     // first commit
 }
